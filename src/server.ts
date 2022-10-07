978edc273dcc47046ef3225874c81be1815681af
gmmunis <gmmunis@hotmail.com>
2022-10-07 16:08:56 -0300

    Cadastro de cliente

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from "express";
+import { routes } from "./routes";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Hello deliveryman"
-    });
-});
+app.use(routes);
 
 app.listen(3000, () => console.log("Server is running"));
